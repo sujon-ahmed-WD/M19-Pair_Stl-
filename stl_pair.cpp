235162a80be5becc190739e926afd79742d9301d
sujon-ahmed-WD <sujon.mrist@gmail.com>
2025-01-29 09:12:46 +0600

    vector_stl

--- stl_pair.cpp
+++ stl_pair.cpp
@@ -2,5 +2,29 @@
 using namespace std;
 int main()
 {
+    //  int n; cin>>n;
+    //  vector<pair<int,int>>v(n);
+    // for(int i=0; i<n; i++)
+    // {
+    //     cin>>v[i].first>>v[i].second;
+    // }
+    // for(int i=0; i<n; i++)
+    // {
+    //     cout<<v[i].first<<v[i].second<<endl;
+    // }
+
+    int n; cin>>n;
+    vector<pair<int,int>>v(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i].first>>v[i].second;
+    }
+    for(int i=0; i<n; i++)
+    {
+        cout<<v[i].first <<" " <<v[i].second<<endl;
+    }
+    
+
+  
     return 0;
 }
